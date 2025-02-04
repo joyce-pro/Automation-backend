870d716f9a607efc937ac9ead825e93834646612
joyce-pro <joyce.aesc@gmail.com>
2025-02-04 10:50:33 +0530

    removing GUI

--- utility/launch-browser.js
+++ utility/launch-browser.js
@@ -7,7 +7,7 @@ const launchBrowser = async () => {
         const options = isLocal
             ? {
                 headless: false,
-                args: ['--no-sandbox', '--disable-setuid-sandbox', '--display=:1'],
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
                 // executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || null, // Local Chrome path
                 userDataDir: path.resolve('./user-data'), // Local directory for persistent data
             }
